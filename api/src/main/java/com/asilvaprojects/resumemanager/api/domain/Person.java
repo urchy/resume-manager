f2f853612ad8c0d7127ff3674763b4ac4a3e1658
Andre Silva <a.silva@techfirm.pt>
2020-09-15 09:37:33 +0100

    upload datamodel  - create person model

---
+++ api/src/main/java/com/asilvaprojects/resumemanager/api/domain/Person.java
@@ -0,0 +1,15 @@
+package com.asilvaprojects.resumemanager.api.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@AllArgsConstructor
+@Builder
+@Document
+public class Person {
+
+
+}
